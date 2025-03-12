3bd1406860a76463bdfd03bfe9da06c9367be3ec
Wallison Caique <84998839+wallisoncaique@users.noreply.github.com>
2025-03-12 15:56:28 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log('Wallison Caique'); /*Forma normal*/
-console.log("Walliosn 'Caique'"); //Qunado precisar por um nome em aspas é só utulizar a aspas diferente da que usou no inicio
-console.log(`'Wallison' "Caique"`); //Usando craze podemos utilizar as aspas simples e dupla no texto
-//os textos são string e os números são nomeados de number
-console.log(123456); //para exibir números não precisa de aspas
-console.log(12, 15.85, 20, 'Wallison'); //usando virgula você pode exibir varios valores sem quebra de linha
