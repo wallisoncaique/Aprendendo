2b5f2adbca2ec481619717ca0657239e5ca74cff
Wallison Caique <84998839+wallisoncaique@users.noreply.github.com>
2025-03-13 16:23:59 -0300

    Assunto da aula, dia 13/03/25 às 16:23.

---
+++ Aula 6 - 13 Variáveis com let/explicação.js
@@ -0,0 +1,19 @@
+let nome; // Declarou a variável
+nome = 'Jão'; // Inicializando a variável
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+
+//Não pode criar variáveis com palavras reservadas ex: let let / let console / let if
+//Mas se usar uma palavra reservadas vai dar erro no código e vou saber do erro
+//Não pode começar o nome de uma variável com um número
+//Bom começar sempre com letra minuscula e também nunca usar espaços ou traços
+
+/*
+Não pode redeclarar uma variável, caso eu coloque novamente: 
+
+let name;
+console.log('teste');
+
+dessa forma vai dar erro e não vai rodar nada do código
+*/
